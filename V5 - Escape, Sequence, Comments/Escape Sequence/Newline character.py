3ebf7410e65e3ded0d8f6dd29caba95d0a572b57
Aryan <aryanfrom2005@gmail.com>
2025-10-01 18:01:17 +0530

    Add files via upload

---
+++ V5 - Escape, Sequence, Comments/Escape Sequence/Newline character.py
@@ -0,0 +1,3 @@
+#Write a Python program to print a multi-line string using the newline escape sequence.
+
+print("Heyy!!! My name is: \nAryan")
---
+++ V5 - Escape, Sequence, Comments/Escape Sequence/Tab Character.py
@@ -0,0 +1,5 @@
+#Create a program that prints a sentence with words separated by a tab space.
+
+sen="Enter the string to be seprated: "
+
+print(sen, sep="\t")
