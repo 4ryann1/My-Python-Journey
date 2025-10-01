3ef6f0063d221b8756667ac5c2213d1ffbaef8c9
Aryan <aryanfrom2005@gmail.com>
2025-10-01 17:57:41 +0530

    Add files via upload

---
+++ V4 - Our First Program/Program1.py
@@ -0,0 +1,9 @@
+print("Hello World!") #Simple String
+print(5) #Printing a number in the form of integer
+print("Hi","Aryan","Patil") #Printing a multiple string output
+
+#Printing commputations
+print(5+1)   #Printing addition
+print(5-1)   #Printing subtraction
+print(5/1)   #Printing division
+print(5*1)   #Printing Multiplication
