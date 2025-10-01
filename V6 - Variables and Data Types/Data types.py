b7b36937f22e0bdbeb4e3faaa81e0a7eca76cd8e
Aryan <aryanfrom2005@gmail.com>
2025-10-02 02:44:21 +0530

    Add files via upload

---
+++ V6 - Variables and Data Types/Data types.py
@@ -0,0 +1,22 @@
+#DataTypes
+
+integer_data=5
+string_data="Aryan"
+boolean_data=True
+complex_data=8+9j #8+9j (also can be written as)
+float_value=3.1428
+
+print(type(integer_data))
+print(type(string_data))
+print(type(boolean_data))
+print(type(complex_data))
+print(type(float_value))
+
+#List
+fruits=["Banana","Apple","Jackfruit","Mango","Papaya"] #List of Fruit
+
+#Tuple
+guys=("Aryan","Bhushan","Mayur","Shiv","Om","Prabhu") #Tuple of Friends
+
+#Dictionary (Mapped Data)
+dict1={"Name":"Aryan","Age":20,"Number":"86258XXXXX","Can Vote":True} #Dictionary
