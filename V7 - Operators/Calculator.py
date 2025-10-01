4f72cadcdc584e4651786d583b9de3cac620d7ee
Aryan <aryanfrom2005@gmail.com>
2025-10-02 02:46:22 +0530

    Add files via upload

---
+++ V7 - Operators/Calculator.py
@@ -0,0 +1,13 @@
+#Calculator Exercise
+#Create a calculator capable of performing addition, subtraction, multiplication and division 
+#operations on two numbers. Your program should format the output in a readable manner.
+
+a=int(input("Enter the value of A: "))
+b=int(input("Enter the value of B: "))
+
+print("The Addition of A & B is: ",a+b)
+print("The Subtraction of A & B is: ",a-b)
+print("The Multiplication of A & B is: ",a*b)
+print("The Division of A & B is: ",a/b)
+print("The Modulus of A & B is: ",a%b)
+print("The Floor Division of A & B is: ",a//b)
---
+++ V7 - Operators/Operators.py
@@ -0,0 +1,8 @@
+#Operators
+
+a=int(input("Enter the Value of A: "))
+b=int(input("Enter the value of B: "))
+print("The addition of the two numbers is:",a+b)
+print("The subtraction of the two numbers is:",a-b)
+print("The multiplication of the two numbers is:",a*b)
+print("The division of the two numbers is:",a/b)
